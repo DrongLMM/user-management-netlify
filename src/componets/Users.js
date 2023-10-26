b98be7baaf731e10a8e8c5fd74158cf96ef5d477
unknown <drong954@gmail.com>
2023-10-26 23:13:00 +0600

    responsive two

--- src/componets/Users.js
+++ src/componets/Users.js
@@ -6,20 +6,22 @@ import { useUserContext } from "../hooks/useContext";
 function Users({ isEdit, setId }) {
   const { state } = useUserContext();
   return (
-    <section>
-      {state.users.map((user) => {
-        return (
-          <User
-            key={user.id}
-            id={user.id}
-            name={user.name}
-            email={user.email}
-            isEdit={isEdit}
-            setId={setId}
-          />
-        );
-      })}
-    </section>
+    <div className="container ">
+      <div className="row">
+        {state.users.map((user) => {
+          return (
+            <User
+              key={user.id}
+              id={user.id}
+              name={user.name}
+              email={user.email}
+              isEdit={isEdit}
+              setId={setId}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
